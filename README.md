4a6d05b251d11f81d9a9f9e532136bf7e0d78487
madisetti <madisetti@jhu.edu>
2021-09-15 19:17:55 -0400

    toki sona

---
+++ README.md
@@ -0,0 +1,49 @@
+# toki sona
+
+a 1000 character language inspired by toki pona
+
+[![tests](https://github.com/dmadisetti/sona.js/actions/workflows/tests.yml/badge.svg)](https://github.com/dmadisetti/sona.js/actions/workflows/tests.yml)
+
+---
+
+![toki sona pi toki sona](https://raw.githubusercontent.com/dmadisetti/sona.js/sona.png)]
+
+### why
+
+why not
+
+### what
+
+`ijo` `<VariableName>` `li` `[Expression]` : Assign Variable
+
+`ken` `[Expression]` `la` `[Expression]` : If then
+
+`ma` `<MarkerName>` : Create goto mark
+
+`tawa` `<MarkerName>` : Goto markername
+
+`nanpa` `[symbols]`: adds number to return stack
+
+`toki` `[symbols]`: adds ascii to return stack
+
+`pini` : Halt the program and returns stack
+
+#### expressions
+
+`ala` `[symbol]`: not symbol
+
+`A` `anu` `B`: bitwise or
+
+`A` `en` `B`: addition
+
+`A` `ante` `B`: subtraction
+
+`A` `mute` `B`: multiplication
+
+`A` `weka` `B`: division
+
+`A` `li` `B`: equality check
+
+### notes
+
+Integer only math. Forgives over fails. Good luck debugging.
---
+++ test/test.js
@@ -0,0 +1,35 @@
+var sona = require("../sona.js");
+
+var assert = require('assert');
+var fs = require("fs");
+
+const load_sona = (file, callback, done) => {
+    fs.readFile(`${__dirname}/${file}`, function(err, data) {
+        if (err) {
+            throw `Unable to read ${file}`;
+        }
+        callback(sona()(data.toString()));
+        done();
+    });
+}
+
+describe('sona', function() {
+    describe('toki', function() {
+        it('says hello', function(done) {
+            load_sona("toki.sona",
+                (result) => assert.deepEqual(result.join(""), "toki! sina pona?"), done);
+        });
+    });
+    describe('nanpa', function() {
+        it('basic number operations', function(done) {
+            load_sona("nanpa.sona",
+                (result) => assert.deepEqual(result, [4, 8, 15, 16, 23, 42, 12, 1, 0, 1337]), done);
+        });
+    });
+    describe('fibonacci', function() {
+        it('computes first 10 fibonacci numbers', function(done) {
+            load_sona("pipinaki.sona",
+                (result) => assert.deepEqual(result, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]), done);
+        });
+    });
+});
